dccb9e7c4cc36b12008048cc95559309d4e303b2
Sahil Shetty <70585867+ACENSONIX@users.noreply.github.com>
2022-05-31 22:59:09 +0530

    fixed login and signup

--- src/components/Pages/Login.js
+++ src/components/Pages/Login.js
@@ -1,88 +1,97 @@
-import React from 'react';
-import { signInWithEmailAndPassword } from 'firebase/auth'
-import { auth } from '../../firebase-config';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase-config";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
-    const [loginEmail, setLoginEmail] = useState("");
-    const [loginPassword, setLoginPassword] = useState("");
-    const navigate = useNavigate();
-    const login = (e) => {
-        // try {
-        //     const user = signInWithEmailAndPassword(auth, loginEmail, loginPassword);
-        //     navigate('/');
-        //     console.log(user);
-        // } catch (error) {
-        //     console.log(error.message);
-        // }
-        e.preventDefault();
-        const user = signInWithEmailAndPassword(auth, loginEmail, loginPassword).then(() => {
-            navigate('/');
-        })
-        .catch((error) => {
-            alert(`Invalid Credentailas`);
-            console.log(error);
-        });
-    };
-    return (
-        <>
-            <div className="w-full max-w-xs">
-                <form onSubmit={login} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                    <div className="mb-4">
-                        <label
-                            className="block text-gray-700 text-sm font-bold mb-2"
-                            htmlFor="username"
-                        >
-                            Username
-                        </label>
-                        <input
-                            onChange={(event) => {
-                                setLoginEmail(event.target.value)
-                            }}
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="username"
-                            type="text"
-                            placeholder="Username"
-                        />
-                    </div>
-                    <div className="mb-6">
-                        <label
-                            className="block text-gray-700 text-sm font-bold mb-2"
-                            htmlFor="password"
-                        >
-                            Password
-                        </label>
-                        <input
-                            onChange={(event) => {
-                                setLoginPassword(event.target.value)
-                            }}
-                            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                            id="password"
-                            type="password"
-                            placeholder="******************"
-                        />
-                        <p className="text-red-500 text-xs italic">Please choose a password.</p>
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <button
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                            type="submit"
-                        >
-                            Sign In
-                        </button>
-                        <a
-                            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                            href="#"
-                        >
-                            Forgot Password?
-                        </a>
-                    </div>
-                </form>
-                <p className="text-center text-gray-500 text-xs">
-                    ©2020 Acme Corp. All rights reserved.
-                </p>
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const navigate = useNavigate();
+  const login = (e) => {
+    // try {
+    //     const user = signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+    //     navigate('/');
+    //     console.log(user);
+    // } catch (error) {
+    //     console.log(error.message);
+    // }
+    e.preventDefault();
+    const user = signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        alert(`Invalid Credentailas`);
+        console.log(error);
+      });
+  };
+  return (
+    <>
+      <div
+        style={{ display: "flex", justifyContent: "center", padding: "100px" }}
+      >
+        <div className="w-full max-w-xs">
+          <form
+            onSubmit={login}
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          >
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                onChange={(event) => {
+                  setLoginEmail(event.target.value);
+                }}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Username"
+              />
             </div>
-        </>
-    );
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                onChange={(event) => {
+                  setLoginPassword(event.target.value);
+                }}
+                className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="******************"
+              />
+              <p className="text-red-500 text-xs italic">
+                Please choose a password.
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Sign In
+              </button>
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                onClick={() => {
+                  navigate("/sign-up");
+                }}
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 export default Login;
--- src/components/Pages/Signup.js
+++ src/components/Pages/Signup.js
@@ -1,28 +1,42 @@
 import React from "react";
-import { createUserWithEmailAndPassword } from 'firebase/auth'
-import { auth } from '../../firebase-config';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase-config";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { collection, addDoc } from 'firebase/firestore'
-import { db } from '../../firebase-config'
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../firebase-config";
 const Signup = () => {
-  const [registerName,setRegisterName] = useState("");
+  const [registerName, setRegisterName] = useState("");
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
   const userCollectionRef = collection(db, "User");
   const navigate = useNavigate();
   const register = (e) => {
     e.preventDefault();
-    const user = createUserWithEmailAndPassword(auth, registerEmail, registerPassword).then(() => {
-      navigate('/');
-      console.log(user);
-    })
+    const user = createUserWithEmailAndPassword(
+      auth,
+      registerEmail,
+      registerPassword
+    )
+      .then(() => {
+        navigate("/");
+        console.log(user);
+      })
       .catch((error) => {
         alert(`User Already Exist`);
         console.log(error);
       });
-    const data = addDoc(userCollectionRef, { Name: registerName, Email: registerEmail }).then(() => { console.log(`Success User Added Successfully`) }).catch((error) => { console.log(error) });
-    // The Below one is the asynchronus function that 
+    const data = addDoc(userCollectionRef, {
+      Name: registerName,
+      Email: registerEmail,
+    })
+      .then(() => {
+        console.log(`Success User Added Successfully`);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    // The Below one is the asynchronus function that
     // try {
     //   const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
     //   console.log(user);
@@ -34,80 +48,77 @@ const Signup = () => {
     <>
       {/* The below line is commented by me */}
       {/* <h1 className="sign-up">Sign Up</h1> */}
-      <div className="w-full max-w-xs">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="name"
-            >
-              Name
-            </label>
-            <input
-              onChange={(event) => {
-                setRegisterName(event.target.value)
-              }}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="name"
-              type="text"
-              placeholder="Name"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="username"
-            >
-              Username
-            </label>
-            <input
-              onChange={(event) => {
-                setRegisterEmail(event.target.value)
-              }}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="text"
-              placeholder="Username"
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              onChange={(event) => {
-                setRegisterPassword(event.target.value);
-              }}
-              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="******************"
-            />
-            <p className="text-red-500 text-xs italic">Please choose a password.</p>
-          </div>
-          <div className="flex items-center justify-between">
-            <button onClick={register}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-            >
-              Sign Up
-            </button>
-            <a
-              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-              href="#"
-            >
-              Forgot Password?
-            </a>
-          </div>
-        </form>
-        <p className="text-center text-gray-500 text-xs">
-          ©2020 Acme Corp. All rights reserved.
-        </p>
+      <div
+        style={{ display: "flex", justifyContent: "center", padding: "100px" }}
+      >
+        <div className="w-full max-w-xs">
+          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="name"
+              >
+                Name
+              </label>
+              <input
+                onChange={(event) => {
+                  setRegisterName(event.target.value);
+                }}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="name"
+                type="text"
+                placeholder="Name"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                onChange={(event) => {
+                  setRegisterEmail(event.target.value);
+                }}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Username"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                onChange={(event) => {
+                  setRegisterPassword(event.target.value);
+                }}
+                className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="******************"
+              />
+              <p className="text-red-500 text-xs italic">
+                Please choose a password.
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                onClick={register}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-
     </>
   );
 };
